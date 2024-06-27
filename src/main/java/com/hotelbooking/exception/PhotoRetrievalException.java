fc6b4d50c332890b4811c9a3c35811dfbcbcc3b5
Nam An <201301962st.hcmuaf.edu.vn>
2024-06-27 10:08:54 +0700

    An

---
+++ src/main/java/com/hotelbooking/exception/PhotoRetrievalException.java
@@ -0,0 +1,11 @@
+package com.hotelbooking.exception;
+
+/**
+ * @author Simpson Alfred
+ */
+
+public class PhotoRetrievalException extends RuntimeException {
+    public PhotoRetrievalException(String message) {
+        super(message);
+    }
+}
